4e4c590e82e044a43a235f628936ca1228e6aab6
Carlos Zelayeta <carloszelayeta@gmail.com>
2025-06-19 12:00:12 -0300

    agrego métodos al backend

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,7 +1,6 @@
 //=======[ Settings, Imports & Data ]==========================================
 
 var PORT    = 3000;
-
 var express = require('express');
 var app     = express();
 var utils   = require('./mysql-connector');
@@ -14,23 +13,50 @@ app.use(express.static('/home/node/app/static/'));
 //=======[ Main module code ]==================================================
 
 app.get('/devices/', function(req, res, next) {
-    devices = [
-        { 
-            'id': 1, 
-            'name': 'Lampara 1', 
-            'description': 'Luz living', 
-            'state': 0, 
-            'type': 1, 
-        },
-        { 
-            'id': 2, 
-            'name': 'Ventilador 1', 
-            'description': 'Ventilador Habitacion', 
-            'state': 1, 
-            'type': 2, 
-        },
-    ]
-    res.send(JSON.stringify(devices)).status(200);
+    utils.query("SELECT * FROM Devices",function(error,respuesta,campos){
+        if(error==null){
+            console.log(respuesta);
+            res.status(200).send(respuesta);    
+        }else{
+            console.log(error);
+            res.status(409).send({error:"Fallo la consulta"});
+        }
+    })
+});
+
+app.post('/devices/', function(req, res, next) {
+    utils.query(
+        "INSERT INTO `Devices` (`id`, `name`, `description`, `state`, `type`) VALUES ("
+        +req.body.id+",'"+req.body.name+"','"+req.body.description+"',"+req.body.state+","+req.body.type+")", 
+        function(error,respuesta,campos){
+        if(error==null){
+            console.log(respuesta);
+            res.status(200).send(respuesta);    
+        }else{
+            console.log(error);
+            res.status(409).send({error:"Fallo la consulta"});
+        }
+    })
+});
+
+app.delete('/devices/',function(req,res,next){
+    utils.query("DELETE FROM Devices where id = "+req.body.id, function(error,respuesta,campos) {
+        if(req.body.id != undefined){
+            res.status(200).send({saludo:"Borro "+req.body.id});
+        }else{
+            res.status(409).send({error:"Falta el id"});
+        }
+    })
+});
+
+app.put('/devices/',function(req,res,next){
+    utils.query("UPDATE Devices SET name ='"+req.body.name+"',description='"+req.body.description+"',type="+req.body.type+" WHERE id ="+req.body.id+";", function(error,respuesta,campos) {
+        if(req.body.id != undefined){
+            res.status(200).send({saludo:"Modifico "+req.body.id});
+        }else{
+            res.status(409).send({error:"Falta el id"});
+        }
+    })
 });
 
 app.listen(PORT, function(req, res) {
